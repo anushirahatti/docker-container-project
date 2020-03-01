ac89654ad1abdf92c85ecdbe3d3f5496c2549c25
EC2 Default User <ec2-user@ip-172-31-81-149.ec2.internal>
2020-03-01 19:22:11 +0000

    updating in docker-container-project

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import click
+from datetime import date
 
 @click.command()
 def hello():
+    today = date.today()
+    format = today.strftime("%b-%d-%Y")
     click.echo('Hello Aniruddha!')
+    print('Today is:', format)
 
 if __name__ == '__main__':
     hello()
